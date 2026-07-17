fcadbf5e004608f438ad6c03558dec735172f497
Vino <vinofernando84@gmail.com>
2026-07-17 19:36:37 +0700

    refactor: hapus direktori lama yang duplikat

--- auth/src/config/websocket.ts
+++
@@ -1,6 +0,0 @@
-import websocket from "@fastify/websocket";
-import type { FastifyInstance } from "fastify";
-
-export const registerWebSocket = async (app: FastifyInstance) => {
-  await app.register(websocket);
-};
