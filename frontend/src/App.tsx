776de94eb256d734636d7856b5f6cbac974e1608
Vino <vinofernando84@gmail.com>
2026-07-19 16:23:57 +0700

    wrap all file with my popup provider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ChatDashbord from "./pages/ChatDashboard";
 import Login from "./pages/Login";
+import { PopupProvider } from "./context/PopupContext";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<ChatDashbord />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
+    <PopupProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<ChatDashbord />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </PopupProvider>
   );
 }
 
